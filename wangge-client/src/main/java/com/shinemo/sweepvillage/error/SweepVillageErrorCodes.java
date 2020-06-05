ea1e2958b16d9fc3aa306231b32b90ede00806ae
zhaozhen <zhaoz@shinemo.com>
2020-06-05 14:57:04 +0800

    c

--- wangge-client/src/main/java/com/shinemo/sweepvillage/error/SweepVillageErrorCodes.java
+++ wangge-client/src/main/java/com/shinemo/sweepvillage/error/SweepVillageErrorCodes.java
@@ -4,7 +4,9 @@ import com.shinemo.common.tools.exception.ErrorCode;
 
 public interface SweepVillageErrorCodes {
     ErrorCode BASE_ERROR = new ErrorCode(500, "系统错误，请稍后尝试");
-    ErrorCode VISIT_RECORDING_NOT_EXIST = new ErrorCode(500, "走访记录不存在或被删除！");
-    ErrorCode SWEEP_VILLAGE_NOT_EXIST = new ErrorCode(500, "扫村计划不存在或已被删除！");
-    ErrorCode VISIT_RECORDING_UPDATE_NOT_AUTH = new ErrorCode(500, "只有录入本人或对应网格长才可以编辑！");
+    ErrorCode VISIT_RECORDING_NOT_EXIST = new ErrorCode(502, "走访记录不存在或被删除！");
+    ErrorCode VISIT_RECORDING_UPDATE_NOT_AUTH = new ErrorCode(503, "权限不足！");
+    ErrorCode NOT_GRID_USER = new ErrorCode(501, "非网格用户！");
+    ErrorCode SWEEP_VILLAGE_ACTIVITY_NOT_EXIST = new ErrorCode(1001, "扫村活动不存在！");
+    ErrorCode SWEEP_VILLAGE_STATUS_ERROR = new ErrorCode(1002, "扫村活动状态错误！");
 }
