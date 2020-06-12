3c83450257c91736fa0ed0bff031bdb23baf5a60
shangkaihui <kaihuishang@163.com>
2020-06-12 13:11:34 +0800

    登录统计功能

--- wangge-web/src/main/java/com/shinemo/wangge/web/sys/controller/SysController.java
+++ wangge-web/src/main/java/com/shinemo/wangge/web/sys/controller/SysController.java
@@ -1,5 +1,6 @@
 package com.shinemo.wangge.web.sys.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020年5月26日 下午6:06:44
  */
 @RestController
+@Slf4j
 public class SysController {
 
     @GetMapping("checkstatus")
     public String checkstatus() {
+        log.info("项目启动成功");
         return "success";
     }
+
 }
