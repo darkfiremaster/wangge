789185094c5edc2642b7b1fdabf8ecc6a63bec71
曾鹏 <2031481080@qq.com>
2020-07-02 18:59:12 +0800

    'fix'

--- wangge-web/src/main/java/com/shinemo/wangge/web/controller/sweepvillage/SweepVillageActivityController.java
+++ wangge-web/src/main/java/com/shinemo/wangge/web/controller/sweepvillage/SweepVillageActivityController.java
@@ -1,12 +1,16 @@
 package com.shinemo.wangge.web.controller.sweepvillage;
 
 import com.shinemo.common.tools.result.ApiResult;
+import com.shinemo.sweepvillage.domain.SweepVillageActivityDO;
+import com.shinemo.sweepvillage.vo.VillageVO;
 import com.shinemo.wangge.core.service.sweepvillage.SweepVillageActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,4 +29,7 @@ public class SweepVillageActivityController {
     public ApiResult<Map<String, Object>> createVillage() {
         return sweepVillageActivityService.createVillage(null);
     }
+
+//    @GetMapping("/getSweepVillageActivityList")
+//    public ApiResult<List<VillageVO>>
 }
