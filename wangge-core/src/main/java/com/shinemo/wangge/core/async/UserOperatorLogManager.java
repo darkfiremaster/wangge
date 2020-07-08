a3fa59429ec6ddc793f5a925065af78d72facfa6
shangkaihui <kaihuishang@163.com>
2020-07-08 09:59:02 +0800

    优化日志插入

--- wangge-core/src/main/java/com/shinemo/wangge/core/async/UserOperatorLogManager.java
+++ wangge-core/src/main/java/com/shinemo/wangge/core/async/UserOperatorLogManager.java
@@ -1,29 +1,29 @@
 package com.shinemo.wangge.core.async;
 
-import java.util.concurrent.TimeUnit;
+import com.shinemo.client.async.log.AbstractAsyncLog;
+import com.shinemo.operate.domain.UserOperateLogDO;
+import com.shinemo.wangge.dal.mapper.UserOperateLogMapper;
 
 import javax.annotation.Resource;
-
-import com.shinemo.client.async.log.AbstractAsyncLog;
-import com.shinemo.operate.vo.UserOperateLogVO;
-import com.shinemo.wangge.core.service.operate.OperateService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author htdong
  * @date 2020年7月7日 下午7:35:42
  */
-public class UserOperatorLogManager extends AbstractAsyncLog<UserOperateLogVO> {
+public class UserOperatorLogManager extends AbstractAsyncLog<UserOperateLogDO> {
+
 
     @Resource
-    private OperateService operateService;
+    private UserOperateLogMapper userOperateLogMapper;
 
     public UserOperatorLogManager(int queueSize, int threadNum, boolean useQueue, long time, TimeUnit unit) {
         super(queueSize, threadNum, useQueue, time, unit);
     }
 
     @Override
-    public void writeLog(UserOperateLogVO tLog) {
-        operateService.addUserOperateLog(tLog);
+    public void writeLog(UserOperateLogDO tLog) {
+        userOperateLogMapper.insert(tLog);
     }
 
 }
