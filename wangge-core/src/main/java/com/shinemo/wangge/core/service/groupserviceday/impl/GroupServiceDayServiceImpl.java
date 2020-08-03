f8dd6472d0f1b88b62894d304bdbdded77fbe922
shangkaihui <kaihuishang@163.com>
2020-08-03 19:15:46 +0800

    上门开户

--- wangge-core/src/main/java/com/shinemo/wangge/core/service/groupserviceday/impl/GroupServiceDayServiceImpl.java
+++ wangge-core/src/main/java/com/shinemo/wangge/core/service/groupserviceday/impl/GroupServiceDayServiceImpl.java
@@ -1,17 +1,29 @@
 package com.shinemo.wangge.core.service.groupserviceday.impl;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import com.shinemo.common.tools.result.ApiResult;
+import com.shinemo.groupserviceday.domain.model.GroupServiceDayDO;
+import com.shinemo.groupserviceday.domain.model.ParentGroupServiceDayDO;
 import com.shinemo.groupserviceday.domain.request.GroupServiceDayPartnerListRequest;
+import com.shinemo.groupserviceday.domain.request.GroupServiceDayRequest;
 import com.shinemo.groupserviceday.domain.request.GroupServiceDaySignRequest;
+import com.shinemo.groupserviceday.enums.GroupServiceDayStatusEnum;
 import com.shinemo.smartgrid.domain.SmartGridContext;
+import com.shinemo.smartgrid.utils.GsonUtils;
 import com.shinemo.wangge.core.service.groupserviceday.GroupServiceDayService;
 import com.shinemo.wangge.core.service.thirdapi.ThirdApiMappingService;
+import com.shinemo.wangge.core.util.HuaWeiUtil;
+import com.shinemo.wangge.core.util.ValidatorUtil;
+import com.shinemo.wangge.dal.mapper.GroupServiceDayMapper;
+import com.shinemo.wangge.dal.mapper.ParentGroupServiceDayMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,6 +37,12 @@ public class GroupServiceDayServiceImpl implements GroupServiceDayService {
 
     @Resource
     private ThirdApiMappingService thirdApiMappingService;
+
+    @Resource
+    private ParentGroupServiceDayMapper parentGroupServiceDayMapper;
+
+    @Resource
+    private GroupServiceDayMapper groupServiceDayMapper;
 
 
     @Override
@@ -41,9 +59,61 @@ public class GroupServiceDayServiceImpl implements GroupServiceDayService {
     }
 
     @Override
-    public ApiResult createGroupServiceDay() {
-        return null;
+    @Transactional
+    public ApiResult<Void> createGroupServiceDay(GroupServiceDayRequest groupServiceDayRequest) {
+        ValidatorUtil.validateEntity(groupServiceDayRequest);
+
+        //生成父活动
+        ParentGroupServiceDayDO parentGroupServiceDayDO = new ParentGroupServiceDayDO();
+        parentGroupServiceDayDO.setTitle(groupServiceDayRequest.getTitle());
+        parentGroupServiceDayDO.setGroupId(groupServiceDayRequest.getGroupId());
+        parentGroupServiceDayDO.setGroupName(groupServiceDayRequest.getGroupName());
+        parentGroupServiceDayDO.setGroupAddress(groupServiceDayRequest.getGroupAddress());
+        parentGroupServiceDayDO.setCreatorId(Long.valueOf(SmartGridContext.getUid()));
+        parentGroupServiceDayDO.setCreatorOrgId(Long.valueOf(SmartGridContext.getOrgId()));
+        parentGroupServiceDayDO.setCreatorName(HuaWeiUtil.getHuaweiUsername(SmartGridContext.getMobile()));
+        parentGroupServiceDayDO.setMobile(SmartGridContext.getMobile());
+        parentGroupServiceDayDO.setPlanStartTime(DateUtil.parseDateTime(groupServiceDayRequest.getPlanStartTime()));
+        parentGroupServiceDayDO.setPlanEndTime(DateUtil.parseDateTime(groupServiceDayRequest.getPlanEndTime()));
+        parentGroupServiceDayDO.setLocation(groupServiceDayRequest.getLocation());
+        parentGroupServiceDayDO.setPartner(GsonUtils.toJson(groupServiceDayRequest.getPartner()));
+        parentGroupServiceDayDO.setStatus(GroupServiceDayStatusEnum.NOT_START.getId());
+        try {
+            parentGroupServiceDayDO.setGridId(SmartGridContext.getSelectGridUserRoleDetail().getId());
+        } catch (Exception e) {
+            log.error("[createGroupServiceDay] 该用户无网格,mobile:{}", SmartGridContext.getMobile());
+            parentGroupServiceDayDO.setGridId("");
+        }
+        parentGroupServiceDayMapper.insert(parentGroupServiceDayDO);
+
+        List<GroupServiceDayRequest.PartnerBean> partnerList = groupServiceDayRequest.getPartner();
+        for (GroupServiceDayRequest.PartnerBean partnerBean : partnerList) {
+            //生成子活动
+            GroupServiceDayDO groupServiceDayDO = new GroupServiceDayDO();
+            groupServiceDayDO.setParentId(parentGroupServiceDayDO.getId());
+            groupServiceDayDO.setTitle(parentGroupServiceDayDO.getTitle());
+            groupServiceDayDO.setGroupId(parentGroupServiceDayDO.getGroupId());
+            groupServiceDayDO.setGroupName(parentGroupServiceDayDO.getGroupName());
+            groupServiceDayDO.setCreatorId(parentGroupServiceDayDO.getCreatorId());
+            groupServiceDayDO.setCreatorName(parentGroupServiceDayDO.getCreatorName());
+            groupServiceDayDO.setPlanStartTime(parentGroupServiceDayDO.getPlanStartTime());
+            groupServiceDayDO.setPlanEntTime(parentGroupServiceDayDO.getPlanEndTime());
+            groupServiceDayDO.setGroupAddress(parentGroupServiceDayDO.getGroupAddress());
+            groupServiceDayDO.setLocation(parentGroupServiceDayDO.getLocation());
+            groupServiceDayDO.setPartner(parentGroupServiceDayDO.getPartner());
+            groupServiceDayDO.setStatus(GroupServiceDayStatusEnum.NOT_START.getId());
+            groupServiceDayDO.setMobile(partnerBean.getMobile());
+            groupServiceDayDO.setName(partnerBean.getName());
+            groupServiceDayDO.setGridId(partnerBean.getGridId());
+            groupServiceDayMapper.insert(groupServiceDayDO);
+
+            //todo 同步华为
+        }
+
+        log.info("[createGroupServiceDay] 新建集团服务日成功, id:{},mobile:{}", parentGroupServiceDayDO.getId(), parentGroupServiceDayDO.getMobile());
+        return ApiResult.of(0);
     }
+
 
     @Override
     public ApiResult getLatestMarketingGroupList() {
