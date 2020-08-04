38bd6f16f26e1d1acad146e5cbc3756f34ff35c9
zhaozhen <zhaoz@shinemo.com>
2020-08-04 10:34:48 +0800

    add sign and end

--- wangge-client/src/main/java/com/shinemo/groupserviceday/domain/request/GroupServiceDaySignRequest.java
+++ wangge-client/src/main/java/com/shinemo/groupserviceday/domain/request/GroupServiceDaySignRequest.java
@@ -1,13 +1,18 @@
 package com.shinemo.groupserviceday.domain.request;
 
+import com.shinemo.sweepfloor.domain.vo.LocationDetailVO;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GroupServiceDaySignRequest {
     /** 活动id */
     private Long id;
-    /** 坐标 */
-    private String location;
-    /** 打卡位置 */
-    private String address;
+    /** 位置信息 */
+    private LocationDetailVO locationDetailVO;
+    /** 备注 */
+    private String remark;
+    /** 图片链接 */
+    private List<String> picUrls;
 }
