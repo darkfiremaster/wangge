10dc8ca0995ec841ca9cffa558ec8001436c5913
shangkaihui <kaihuishang@163.com>
2020-08-05 10:46:31 +0800

    Revert "码店跳转增加活动Id"
    This reverts commit f5f4d12d

--- wangge-core/src/main/java/com/shinemo/wangge/core/handler/MaDianAdHandler.java
+++ wangge-core/src/main/java/com/shinemo/wangge/core/handler/MaDianAdHandler.java
@@ -20,13 +20,18 @@ public class MaDianAdHandler extends MaDianCommonHandler {
     @Setter
     protected String path;
 
-    private static final String body = "{\"request\":{\"body\":{\"userPhone\":\"%s\",\"adType\":%s,\"activityId\":\"%s\"},\"header\":{\"appId\":\"%s\"}}}";
+    private static final String body = "{\"request\":{\"body\":{\"userPhone\":\"%s\",\"adType\":%s,\"activityId\":%s},\"header\":{\"appId\":\"%s\"}}}";
 
     @Override
     public String getUrl(UrlRedirectHandlerRequest request) {
         MaDianParams maDianParams = request.getBizParams();
         Integer adType = maDianParams.getAdType();
-        String requestBody = String.format(body, request.getUserPhone(), adType, StrUtil.isNotBlank(request.getActivityStrId()) ? request.getActivityStrId() : "", appId);
+        String requestBody;
+        if (StrUtil.isNotBlank(request.getActivityStrId())) {
+            requestBody = String.format(body, request.getUserPhone(), adType, request.getActivityStrId(), appId);
+        } else {
+            requestBody = String.format(body, request.getUserPhone(), adType, "", appId);
+        }
         StringBuilder url = new StringBuilder();
         url.append(domain)
                 .append(path)
