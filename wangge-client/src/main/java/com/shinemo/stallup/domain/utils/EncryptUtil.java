d81f1b07b0ed46427b33b486a2f003236bfe3b95
shangkaihui <kaihuishang@163.com>
2020-08-05 19:21:22 +0800

    fix

--- wangge-client/src/main/java/com/shinemo/stallup/domain/utils/EncryptUtil.java
+++ wangge-client/src/main/java/com/shinemo/stallup/domain/utils/EncryptUtil.java
@@ -3,6 +3,7 @@ package com.shinemo.stallup.domain.utils;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -70,11 +71,17 @@ public class EncryptUtil {
             }else{
                 sb.append(key);
             }
-            sb.append("=").append(URLEncoder.encode(String.valueOf(params.get(key)))).append("&");
+            if (params.get(key) instanceof Map) {
+                sb.append("=").append(URLEncoder.encode(StringUtils.trimAllWhitespace(params.get(key).toString()))).append("&");
+            } else {
+                sb.append("=").append(URLEncoder.encode(String.valueOf(params.get(key)))).append("&");
+            }
         }
         String str = sb.substring(0, sb.length()-1);
         return str;
     }
+
+
 
     public static String buildParameterString(Map<String, Object> param) {
         String paramString = sortString(param, true);
@@ -227,7 +234,11 @@ public class EncryptUtil {
     }
 
     public static void main(String[] args) {
-        test();
+        Map<String, String> map = new HashMap();
+        map.put("name", "skh");
+        map.put("age", "10");
+        String s = StringUtils.trimAllWhitespace(map.toString());
+        System.out.println("s = " + s);
     }
 
 }
