836cb51b024d277beb30a0742507f4eb0907e805
shangkaihui <kaihuishang@163.com>
2020-08-06 16:11:22 +0800

    fix

--- wangge-core/src/main/java/com/shinemo/wangge/core/schedule/LoginInfoSchedule.java
+++ wangge-core/src/main/java/com/shinemo/wangge/core/schedule/LoginInfoSchedule.java
@@ -5,7 +5,6 @@ import com.shinemo.operate.domain.LoginInfoResultDO;
 import com.shinemo.wangge.core.service.operate.LoginStatisticsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -22,7 +21,7 @@ public class LoginInfoSchedule {
     @Autowired
     private LoginStatisticsService loginStatisticsService;
 
-    @Scheduled(cron = "0 0 1 * * ?")
+    //@Scheduled(cron = "0 0 1 * * ?")
     public void generateLoginInfoResult() {
         log.info("[generateLoginInfoResult] 开始统计登录信息, ");
         long startTime = System.currentTimeMillis();
