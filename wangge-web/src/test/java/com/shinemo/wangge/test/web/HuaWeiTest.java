15c6c6dab839b167ed34b3ddc17d67a4f61652a9
shangkaihui <kaihuishang@163.com>
2020-08-13 18:28:24 +0800

    fix push msg

---
+++ wangge-web/src/test/java/com/shinemo/wangge/test/web/HuaWeiTest.java
@@ -0,0 +1,43 @@
+package com.shinemo.wangge.test.web;
+
+import com.shinemo.smartgrid.http.HttpConnectionUtils;
+import com.shinemo.smartgrid.http.HttpResult;
+import com.shinemo.smartgrid.utils.GsonUtils;
+import com.shinemo.smartgrid.utils.SmartGridUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author shangkaihui
+ * @Date 2020/8/13 17:37
+ * @Desc
+ */
+@Slf4j
+public class HuaWeiTest {
+
+    //@Value("${smartgrid.huawei.accessKeyId}")
+    public String accessKeyId = "xmsystem";
+    //@Value("${smartgrid.huawei.secretKey}")
+    public String secretKey = "7CD36D02A911BFDF5AFA1C91C0A319B8";
+    //@Value("${smartgrid.huawei.domain}")
+    public String domain = "http://112.54.48.61:13003";
+
+    @Test
+    public void test1() {
+        String mobile = "13607713224";
+        String huaweiUrl = "/SGCoreMarketing/groupService/getGroupList";
+
+        Map<String, Object> header = SmartGridUtils.buildHeader(mobile, accessKeyId, secretKey);
+        log.info("header:{}", header);
+
+        HashMap<String, Object> requestData = new HashMap<>();
+        String param = GsonUtils.toJson(requestData);
+        log.info("param:{}", param);
+
+        HttpResult httpResult = HttpConnectionUtils.httpPost(domain + huaweiUrl, param, header);
+        log.info("httpResult:{}", httpResult);
+    }
+}
