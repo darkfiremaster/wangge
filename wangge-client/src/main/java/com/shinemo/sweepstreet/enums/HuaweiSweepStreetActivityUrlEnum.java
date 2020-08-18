32355395dcce990c400cfc9aa4c0d8ac448bde31
曾鹏 <2031481080@qq.com>
2020-08-18 14:55:04 +0800

    'fix'

--- wangge-client/src/main/java/com/shinemo/sweepstreet/enums/HuaweiSweepStreetActivityUrlEnum.java
+++ wangge-client/src/main/java/com/shinemo/sweepstreet/enums/HuaweiSweepStreetActivityUrlEnum.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public enum HuaweiSweepStreetActivityUrlEnum {
     CREATE_SWEEP_STREET_ACTIVITY("/SGCoreMarketing/XX/XX","新建扫街活动","",""),
+    ADD_OR_UPDATE_SWEEP_STREET_ACTIVITY_DATA("ADD_OR_UPDATE_GROUP_SERVICE_DAY_DATA","新增/编辑业务办理量","","saveStreetScanBiz")
     ;
 
     private String url;
