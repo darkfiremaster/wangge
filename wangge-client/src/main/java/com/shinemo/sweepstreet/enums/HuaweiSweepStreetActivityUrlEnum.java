3c87fb1d2150ff0fe5ee545d24a9acb8c91a2fa8
chenqinhai <471927926@qq.com>
2020-08-18 15:33:48 +0800

    fix

--- wangge-client/src/main/java/com/shinemo/sweepstreet/enums/HuaweiSweepStreetActivityUrlEnum.java
+++ wangge-client/src/main/java/com/shinemo/sweepstreet/enums/HuaweiSweepStreetActivityUrlEnum.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @Getter
 public enum HuaweiSweepStreetActivityUrlEnum {
     CREATE_SWEEP_STREET_ACTIVITY("/SGCoreMarketing/XX/XX","新建扫街活动","",""),
-    ADD_OR_UPDATE_SWEEP_STREET_ACTIVITY_DATA("ADD_OR_UPDATE_GROUP_SERVICE_DAY_DATA","新增/编辑业务办理量","","saveStreetScanBiz")
+    ADD_OR_UPDATE_SWEEP_STREET_ACTIVITY_DATA("ADD_OR_UPDATE_GROUP_SERVICE_DAY_DATA","新增/编辑业务办理量","","saveStreetScanBiz"),
+    CREATE_SWEEP_STREET_VISITRECORD("addVisitRecord","新增走访记录","","addVisitRecord")
     ;
 
     private String url;
