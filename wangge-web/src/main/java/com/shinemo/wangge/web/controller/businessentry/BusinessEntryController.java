4790ae7cdd66793de9faeab32018278463fe105f
zhaozhen <zhaoz@shinemo.com>
2020-09-01 11:41:56 +0800

    c

---
+++ wangge-web/src/main/java/com/shinemo/wangge/web/controller/businessentry/BusinessEntryController.java
@@ -0,0 +1,12 @@
+package com.shinemo.wangge.web.controller.businessentry;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("businessEntry")
+@Slf4j
+public class BusinessEntryController {
+}
